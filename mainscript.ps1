ee4f083f378f01e37c096c4f7f1e0397f0c5c3e6
Matthias-Schulski <67123676+Matthias-Schulski@users.noreply.github.com>
2024-05-30 16:42:10 +0200

    Create mainscript.ps1

---
+++ mainscript.ps1
@@ -0,0 +1,6 @@
+//Tell the student what the script does
+
+//Ask for student name and student number
+
+//Load the menu to select a course
+//Dynamicaly loads all the cources from the ./courses directory
