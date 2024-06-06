d9b9efeb8cf0da3bacec91b847f1d171b56c776d
simoneverhagen <157530626+simoneverhagen@users.noreply.github.com>
2024-06-06 18:53:27 +0200

    install_windowspowertoys.ps1

--- infra/windows/applications/install_windowspowertoys.ps1
+++ infra/windows/applications/install_windowspowertoys.ps1
@@ -1,1 +1,18 @@
+# Controleer of Chocolatey is geïnstalleerd
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host "Chocolatey is niet geïnstalleerd. Het wordt nu geïnstalleerd..."
+    
+    # Installeer Chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force; `
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; `
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    
+    # Controleer opnieuw of Chocolatey is geïnstalleerd
+    if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+        Write-Host "Er is een probleem opgetreden bij het installeren van Chocolatey."
+        exit
+    }
+}
 
+# Installeer Windows PowerToys met Chocolatey
+choco install powertoys -y
