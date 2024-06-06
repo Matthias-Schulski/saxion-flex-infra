7dabda3595d827dbff66f2c09182e7ea48dee82f
simoneverhagen <157530626+simoneverhagen@users.noreply.github.com>
2024-06-06 18:09:26 +0200

    install_chrome.ps1

--- infra/windows/applications/install_chrome.ps1
+++ infra/windows/applications/install_chrome.ps1
@@ -1,11 +1,14 @@
-# Specify the URL for the Chrome installer
-$chromeUrl = "https://dl.google.com/chrome/install/ChromeStandaloneSetup64.exe"
+# Set the variable $Path to the location of the temporary folder
+$Path = $env:TEMP; 
 
-# Define the path where you want to save the installer
-$installerPath = "$PSScriptRoot\ChromeStandaloneSetup64.exe"
+# Set the variable $Installer to the name of the installer file
+$Installer = "chrome_installer.exe"; 
 
-# Download the Chrome installer
-Invoke-WebRequest -Uri $chromeUrl -OutFile $installerPath
+# Download the Chrome installer from the specified URL and save it in the temporary folder with the specified name
+Invoke-WebRequest -Uri "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path\$Installer; 
 
-# Install Chrome silently
-Start-Process -FilePath $installerPath -ArgumentList "/silent /install" -Verb RunAs
+# Start the installer with silent installation and elevated permissions
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; 
+
+# Delete the installer file after installation is complete
+Remove-Item -Path $Path\$Installer
