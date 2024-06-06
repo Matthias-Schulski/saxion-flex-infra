d92d5c1292cdebf72e0ab26a226f8a24385a5387
simoneverhagen <157530626+simoneverhagen@users.noreply.github.com>
2024-06-06 18:41:16 +0200

    install_notepad++.ps1

--- infra/windows/applications/install_notepad++.ps1
+++ infra/windows/applications/install_notepad++.ps1
@@ -1,1 +1,14 @@
+# URL van de Notepad++ installer
+$url = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2.1/npp.8.2.1.Installer.exe"
 
+# Locatie waar de Notepad++ installer wordt gedownload
+$output = "$env:TEMP\npp_installer.exe"
+
+# Download de Notepad++ installer van de opgegeven URL
+Invoke-WebRequest -Uri $url -OutFile $output
+
+# Start de installatie van Notepad++
+Start-Process -FilePath $output -ArgumentList "/S" -Wait
+
+# Verwijder de gedownloade installer na installatie
+Remove-Item -Path $output
