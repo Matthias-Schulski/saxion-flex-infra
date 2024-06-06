0d11616105e9b6d4977a35e99fac458156be56b4
simoneverhagen <157530626+simoneverhagen@users.noreply.github.com>
2024-06-06 18:42:33 +0200

    install_firefox.ps1

--- infra/windows/applications/install_firefox.ps1
+++ infra/windows/applications/install_firefox.ps1
@@ -1,1 +1,14 @@
+# URL van de Firefox installer
+$url = "https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US"
 
+# Locatie waar de Firefox installer wordt gedownload
+$output = "$env:TEMP\FirefoxInstaller.exe"
+
+# Download de Firefox installer van de opgegeven URL
+Invoke-WebRequest -Uri $url -OutFile $output
+
+# Start de installatie van Firefox
+Start-Process -FilePath $output -ArgumentList "/S" -Wait
+
+# Verwijder de gedownloade installer na installatie
+Remove-Item -Path $output
