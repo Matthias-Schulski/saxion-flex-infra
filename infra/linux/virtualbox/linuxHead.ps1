a8ec57fd7f7f7a5c0a2e6a3db0e1b051439be744
Stefanfrijns <153369666+Stefanfrijns@users.noreply.github.com>
2024-06-11 22:29:44 +0200

    Update linuxHead.ps1
    Update

--- infra/linux/virtualbox/linuxHead.ps1
+++ infra/linux/virtualbox/linuxHead.ps1
@@ -1,13 +1,17 @@
 param (
-    [string]$studentNumber,
-    [string]$configPath
+    [string]$VMName,
+    [string]$VHDUrl,
+    [string]$OSType,
+    [int]$MemorySize,
+    [int]$CPUs,
+    [string]$NetworkType,
+    [string]$Applications
 )
 
 # Tijdelijk wijzigen van de Execution Policy om het uitvoeren van scripts toe te staan
 $previousExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
-[string]$VHDLinksUrl = "https://raw.githubusercontent.com/Matthias-Schulski/saxion-flex-infra/main/courses/harddisks.json"
 [string]$ConfigureNetworkUrl = "https://raw.githubusercontent.com/Matthias-Schulski/saxion-flex-infra/main/infra/linux/virtualbox/ConfigureNetwork.ps1"
 [string]$CreateVM1Url = "https://raw.githubusercontent.com/Stefanfrijns/HBOICT/main/Virtualbox/CreateVM1.ps1"
 [string]$ModifyVMSettingsUrl = "https://raw.githubusercontent.com/Stefanfrijns/HBOICT/main/Virtualbox/ModifyVMSettings.ps1"
@@ -28,20 +32,6 @@ function Download-File {
     }
 }
 
-# Functie om het OS-type te bepalen
-function Get-OSType {
-    param (
-        [string]$osName
-    )
-    if ($osName -match "Ubuntu") {
-        return "Ubuntu_64"
-    } elseif ($osName -match "Debian") {
-        return "Debian_64"
-    } else {
-        return "Other_64"
-    }
-}
-
 # Controleer of VBoxManage beschikbaar is
 $vboxManagePath = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 if (-not (Test-Path $vboxManagePath)) {
@@ -50,27 +40,15 @@ if (-not (Test-Path $vboxManagePath)) {
 }
 
 # Lokale paden voor de gedownloade bestanden
-$vhdLinksLocalPath = "$env:Public\Downloads\linux_vhdlink.json"
 $configureNetworkLocalPath = "$env:Public\Downloads\ConfigureNetwork.ps1"
 $createVM1LocalPath = "$env:Public\Downloads\CreateVM1.ps1"
 $modifyVMSettingsLocalPath = "$env:Public\Downloads\ModifyVMSettings.ps1"
 $createdVMsPath = "$env:Public\created_vms.txt"
 
 # Download de JSON-bestanden en de scripts
-Download-File -url $VHDLinksUrl -output $vhdLinksLocalPath
 Download-File -url $ConfigureNetworkUrl -output $configureNetworkLocalPath
 Download-File -url $CreateVM1Url -output $createVM1LocalPath
 Download-File -url $ModifyVMSettingsUrl -output $modifyVMSettingsLocalPath
-
-# Lees de JSON configuratie
-$config = Get-Content $configPath -Raw | ConvertFrom-Json
-$vhdLinks = Get-Content $vhdLinksLocalPath -Raw | ConvertFrom-Json
-
-# Map to store OS to VHD URL
-$vhdUrlMap = @{}
-foreach ($vhdLink in $vhdLinks) {
-    $vhdUrlMap[$vhdLink.OS] = $vhdLink.VHDUrl
-}
 
 # Controleer of het bestand met aangemaakte VM's bestaat
 if (-not (Test-Path $createdVMsPath)) {
@@ -84,75 +62,63 @@ $createdVMs = $createdVMs -split "`n" | ForEach-Object { $_.Trim() } | Where-Obj
 Write-Output "List of created VMs:"
 $createdVMs | ForEach-Object { Write-Output " - $_" }
 
-foreach ($vm in $config.VMs) {
-    $vmName = "$($vm.VMName)_$studentNumber".Trim()
-    Write-Output "Checking for VM: '$vmName'"
-    $osTypeKey = $vm.VMVHDFile  # Use VMVHDFile field to determine the OS type
-    $VHDUrl = $vhdUrlMap[$osTypeKey]
-    if (-not $VHDUrl) {
-        Write-Output "VHD URL not found for $osTypeKey. Skipping VM creation for $vmName."
-        continue
-    }
-    $OSType = Get-OSType -osName $osTypeKey
-    $MemorySize = 2048  # Default memory size, change logic if needed
-    $CPUs = 2  # Default CPU count, change logic if needed
-    $NetworkType = $vm.VMNetworkType
+Write-Output "Checking for VM: '$VMName'"
 
-    # Check if the VM already exists
-    $vmExists = $false
-    foreach ($createdVM in $createdVMs) {
-        Write-Output "Comparing '$vmName' with '$createdVM'"
-        if ($createdVM.Trim() -eq $vmName) {
-            Write-Output "Found existing VM: '$createdVM'"
-            $vmExists = $true
-            break
+# Check if the VM already exists
+$vmExists = $false
+foreach ($createdVM in $createdVMs) {
+    Write-Output "Comparing '$VMName' with '$createdVM'"
+    if ($createdVM.Trim() -eq $VMName) {
+        Write-Output "Found existing VM: '$createdVM'"
+        $vmExists = $true
+        break
+    }
+}
+
+if ($vmExists) {
+    Write-Output "VM $VMName already exists. Checking if it's running."
+    $vmState = & "$vboxManagePath" showvminfo "$VMName" --machinereadable | Select-String -Pattern "^VMState=" | ForEach-Object { $_.Line.Split("=")[1].Trim('"') }
+    if ($vmState -eq "running") {
+        Write-Output "VM $VMName is already running. Prompting user for permission to shut down."
+        $userInput = Read-Host "VM $VMName is currently running. Do you want to shut it down to apply changes? (yes/no)"
+        if ($userInput -eq "yes") {
+            & "$vboxManagePath" controlvm $VMName acpipowerbutton
+            Start-Sleep -Seconds 10
+        } else {
+            Write-Output "Skipping changes for VM $VMName."
+            exit
         }
     }
+    # Call the script to modify the VM settings
+    $arguments = @(
+        "-VMName", $VMName,
+        "-MemorySize", $MemorySize,
+        "-CPUs", $CPUs,
+        "-NetworkType", $NetworkType,
+        "-ConfigureNetworkPath", $configureNetworkLocalPath,
+        "-Applications", $Applications
+    )
+    & pwsh -File $modifyVMSettingsLocalPath @arguments
+} else {
+    Write-Output "Creating new VM: $VMName"
+    # Roep het CreateVM1.ps1 script aan met de juiste parameters
+    $arguments = @(
+        "-VMName", $VMName,
+        "-VHDUrl", $VHDUrl,
+        "-OSType", $OSType,
+        "-MemorySize", $MemorySize,
+        "-CPUs", $CPUs,
+        "-NetworkType", $NetworkType,
+        "-ConfigureNetworkPath", $configureNetworkLocalPath,
+        "-Applications", $Applications
+    )
+    & pwsh -File $createVM1LocalPath @arguments
 
-    if ($vmExists) {
-        Write-Output "VM $vmName already exists. Checking if it's running."
-        $vmState = & "$vboxManagePath" showvminfo "$vmName" --machinereadable | Select-String -Pattern "^VMState=" | ForEach-Object { $_.Line.Split("=")[1].Trim('"') }
-        if ($vmState -eq "running") {
-            Write-Output "VM $vmName is already running. Prompting user for permission to shut down."
-            $userInput = Read-Host "VM $vmName is currently running. Do you want to shut it down to apply changes? (yes/no)"
-            if ($userInput -eq "yes") {
-                & "$vboxManagePath" controlvm $vmName acpipowerbutton
-                Start-Sleep -Seconds 10
-            } else {
-                Write-Output "Skipping changes for VM $vmName."
-                continue
-            }
-        }
-        # Call the script to modify the VM settings
-        $arguments = @(
-            "-VMName", $vmName,
-            "-MemorySize", $MemorySize,
-            "-CPUs", $CPUs,
-            "-NetworkType", $NetworkType,
-            "-ConfigureNetworkPath", $configureNetworkLocalPath
-        )
-        & pwsh -File $modifyVMSettingsLocalPath @arguments
-    } else {
-        Write-Output "Creating new VM: $vmName"
-        # Roep het CreateVM1.ps1 script aan met de juiste parameters
-        $arguments = @(
-            "-VMName", $vmName,
-            "-VHDUrl", $VHDUrl,
-            "-OSType", $OSType,
-            "-MemorySize", $MemorySize,
-            "-CPUs", $CPUs,
-            "-NetworkType", $NetworkType,
-            "-ConfigureNetworkPath", $configureNetworkLocalPath
-        )
-        & pwsh -File $createVM1LocalPath @arguments
-
-        # Voeg de naam van de aangemaakte VM toe aan created_vms.txt
-        Add-Content -Path $createdVMsPath -Value $vmName
-    }
+    # Voeg de naam van de aangemaakte VM toe aan created_vms.txt
+    Add-Content -Path $createdVMsPath -Value $VMName
 }
 
 # Herstellen van de oorspronkelijke Execution Policy
 Set-ExecutionPolicy -ExecutionPolicy $previousExecutionPolicy -Scope Process -Force
 
 Write-Output "Script execution completed successfully."
-echo test
