f3c3694a896b068c30b2a252ec137d7022f6349c
Stefanfrijns <153369666+Stefanfrijns@users.noreply.github.com>
2024-06-18 18:49:25 +0200

    Add files via upload

---
+++ infra/linux/virtualbox/installApplications1.3.ps1
@@ -0,0 +1,53 @@
+﻿### PARAMETERS
+param (
+[STRING]$username    = "",         #DEFAULT USER
+[STRING]$password    = "",         #DEFAULT WACHTWOORD
+[STRING]$hostname    = "",
+[STRING]$vmname      = "",
+[STRING]$vmapplications = ""
+)
+
+$counter     = 0
+$scriptsPath = (Join-Path -Path $PSScriptRoot -ChildPath "SCRIPTS")
+$applCounter = 0
+    
+vboxmanage startvm $vmname
+write-host "Guestadditions wordt geïnstalleerd op $vmname. Wanneer gevraagd wordt om het wachtwoord, vul het wachtwoord $password in."
+ssh -p 2222 -t ubuntu@127.0.0.1 'sudo apt install -y bzip2 tar; sudo mount /dev/cdrom /mnt; cd /mnt; sudo sh ./VBoxLinuxAdditions.run'
+
+    #DIRECTORY AANMAKEN VOOR SCRIPTS IN VM
+    write-host "Directory aanmaken in VM (scripts)" -ForegroundColor Yellow
+    VboxManage guestcontrol $($VM.VMName) mkdir "/home/$hostname/scripts" --username ubuntu --password ubuntu
+    Write-Host "Directory aangemaakt" -ForegroundColor Green
+
+    #TE INSTALLEREN SCRIPTS LATEN ZIEN
+    write-host "De volgende applicaties zullen geïnstalleerd worden op deze virtuele machine:" -ForegroundColor Yellow
+    Write-Host "VM Name: $vmname" -ForegroundColor Cyan
+    foreach ($app in $vmapplications) {
+        $applCounter++
+        Write-Host "$app" -ForegroundColor Green
+    }
+
+
+    ###### SCRIPTS AANROEPEN WANNEER PAD BESTAAT ########
+    foreach ($app in $vm.VMApplications) {
+        $appFolderPath = Join-Path -Path $scriptsPath -ChildPath $app
+    
+        if (Test-Path $appFolderPath -PathType Container) {
+            $scripts = Get-ChildItem -Path $appFolderPath -Filter "*.ps1" -File
+        
+            foreach ($script in $scripts) {
+                #PARAMETERS MEEGEVEN VOOR ALLE INSTALLSCRIPTS
+                $arguments = @(
+                    "-VMName", $vmname,
+                    "-Username", $Username,
+                    "-Password", $Password
+                )
+            
+                #AANROEPEN INSTALLSCRIPTS MET PARAMETERS.
+                & pwsh -File $script.FullName @arguments
+            }
+        } else {
+            Write-Host "Geen scripts gevonden voor applicatie $app" -ForegroundColor DarkRed
+        }
+    }
