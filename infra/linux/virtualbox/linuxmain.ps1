f266ac220203ea7cb2c81d5ab3f12f97d5db7ba3
Stefanfrijns <153369666+Stefanfrijns@users.noreply.github.com>
2024-06-21 10:38:23 +0200

    Update and rename linuxHead.ps1 to linuxmain.ps1

--- infra/linux/virtualbox/linuxmain.ps1
+++ infra/linux/virtualbox/linuxmain.ps1
@@ -2,18 +2,24 @@ param (
     [string]$VMName,
     [string]$VHDUrl,
     [string]$OSType,
+    [string]$DistroName,
     [int]$MemorySize,
     [int]$CPUs,
-    [string]$NetworkType,
-    [string]$Applications
+    [string]$NetworkTypes,  # JSON-string
+    [string]$Applications,
+    [string]$ConfigureNetworkPath
 )
 
 # Tijdelijk wijzigen van de Execution Policy om het uitvoeren van scripts toe te staan
 $previousExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
-[string]$ConfigureNetworkUrl = "https://raw.githubusercontent.com/Matthias-Schulski/saxion-flex-infra/main/infra/linux/virtualbox/ConfigureNetwork.ps1"
-[string]$CreateVM1Url = "https://raw.githubusercontent.com/Stefanfrijns/HBOICT/main/Virtualbox/CreateVM1.ps1"
+# Tijdelijk wijzigen van de Execution Policy om het uitvoeren van scripts toe te staan
+$previousExecutionPolicy = Get-ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+[string]$ConfigureNetworkUrl = "https://raw.githubusercontent.com/Stefanfrijns/HBOICT/main/test6/configurenetwork1.ps1"
+[string]$CreateVM1Url = "https://raw.githubusercontent.com/Stefanfrijns/HBOICT/main/test6/createvm.ps1"
 [string]$ModifyVMSettingsUrl = "https://raw.githubusercontent.com/Stefanfrijns/HBOICT/main/Virtualbox/ModifyVMSettings.ps1"
 
 # Functie om een bestand te downloaden
@@ -22,35 +28,53 @@ function Download-File {
         [string]$url,
         [string]$output
     )
-    try {
-        $client = New-Object System.Net.WebClient
-        $client.DownloadFile($url, $output)
-        Write-Output "Downloaded file from $url to $output"
-    } catch {
-        Write-Output "Failed to download file from $url to $output"
-        throw
+    if (-not (Test-Path $output)) {
+        try {
+            $client = New-Object System.Net.WebClient
+            $client.DownloadFile($url, $output)
+            Write-Output "Downloaded file from $url to $output"
+        } catch {
+            Write-Output "Failed to download file from $url to $output"
+            throw
+        }
+    } else {
+        Write-Output "File already exists: $output. Skipping download."
     }
+}
+
+# Log functie
+$logFilePath = "$env:Public\LinuxMainScript.log"
+function Log-Message {
+    param (
+        [string]$message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "$timestamp - $message"
+    Write-Output $logMessage
+    Add-Content -Path $logFilePath -Value $logMessage
 }
 
 # Controleer of VBoxManage beschikbaar is
 $vboxManagePath = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 if (-not (Test-Path $vboxManagePath)) {
-    Write-Output "VBoxManage not found. Ensure VirtualBox is installed."
+    Log-Message "VBoxManage not found. Ensure VirtualBox is installed."
     throw "VBoxManage not found."
 }
 
 # Lokale paden voor de gedownloade bestanden
-$configureNetworkLocalPath = "$env:Public\Downloads\ConfigureNetwork.ps1"
+$configureNetworkPath = "$env:Public\Downloads\ConfigureNetwork.ps1"
 $createVM1LocalPath = "$env:Public\Downloads\CreateVM1.ps1"
 $modifyVMSettingsLocalPath = "$env:Public\Downloads\ModifyVMSettings.ps1"
 $createdVMsPath = "$env:Public\created_vms.txt"
 
 # Download de JSON-bestanden en de scripts
-Download-File -url $ConfigureNetworkUrl -output $configureNetworkLocalPath
+Download-File -url $ConfigureNetworkUrl -output $configureNetworkPath
 Download-File -url $CreateVM1Url -output $createVM1LocalPath
 Download-File -url $ModifyVMSettingsUrl -output $modifyVMSettingsLocalPath
 
+
 # Controleer of het bestand met aangemaakte VM's bestaat
+$createdVMsPath = "$env:Public\created_vms.txt"
 if (-not (Test-Path $createdVMsPath)) {
     New-Item -ItemType File -Force -Path $createdVMsPath
 }
@@ -67,25 +91,25 @@ Write-Output "Checking for VM: '$VMName'"
 # Check if the VM already exists
 $vmExists = $false
 foreach ($createdVM in $createdVMs) {
-    Write-Output "Comparing '$VMName' with '$createdVM'"
+    Log-Message "Comparing '$VMName' with '$createdVM'"
     if ($createdVM.Trim() -eq $VMName) {
-        Write-Output "Found existing VM: '$createdVM'"
+        Log-Message "Found existing VM: '$createdVM'"
         $vmExists = $true
         break
     }
 }
 
 if ($vmExists) {
-    Write-Output "VM $VMName already exists. Checking if it's running."
+    Log-Message "VM $VMName already exists. Checking if it's running."
     $vmState = & "$vboxManagePath" showvminfo "$VMName" --machinereadable | Select-String -Pattern "^VMState=" | ForEach-Object { $_.Line.Split("=")[1].Trim('"') }
     if ($vmState -eq "running") {
-        Write-Output "VM $VMName is already running. Prompting user for permission to shut down."
+        Log-Message "VM $VMName is already running. Prompting user for permission to shut down."
         $userInput = Read-Host "VM $VMName is currently running. Do you want to shut it down to apply changes? (yes/no)"
         if ($userInput -eq "yes") {
             & "$vboxManagePath" controlvm $VMName acpipowerbutton
-            Start-Sleep -Seconds 15
+            Start-Sleep -Seconds 10
         } else {
-            Write-Output "Skipping changes for VM $VMName."
+            Log-Message "Skipping changes for VM $VMName."
             exit
         }
     }
@@ -94,23 +118,24 @@ if ($vmExists) {
         "-VMName", $VMName,
         "-MemorySize", $MemorySize,
         "-CPUs", $CPUs,
-        "-NetworkType", $NetworkType,
-        "-ConfigureNetworkPath", $configureNetworkLocalPath,
-        "-Applications", $Applications
+        "-NetworkTypes", $NetworkTypes,
+        "-Applications", $Applications,
+        "-ConfigureNetworkPath", $configureNetworkPath
     )
     & pwsh -File $modifyVMSettingsLocalPath @arguments
 } else {
-    Write-Output "Creating new VM: $VMName"
+    Log-Message "Creating new VM: $VMName"
     # Roep het CreateVM1.ps1 script aan met de juiste parameters
     $arguments = @(
         "-VMName", $VMName,
         "-VHDUrl", $VHDUrl,
         "-OSType", $OSType,
+        "-DistroName", $DistroName,
         "-MemorySize", $MemorySize,
         "-CPUs", $CPUs,
-        "-NetworkType", $NetworkType,
-        "-ConfigureNetworkPath", $configureNetworkLocalPath,
-        "-Applications", $Applications
+        "-NetworkTypes", $NetworkTypes,
+        "-Applications", $Applications,
+        "-ConfigureNetworkPath", $configureNetworkPath
     )
     & pwsh -File $createVM1LocalPath @arguments
 
@@ -121,4 +146,4 @@ if ($vmExists) {
 # Herstellen van de oorspronkelijke Execution Policy
 Set-ExecutionPolicy -ExecutionPolicy $previousExecutionPolicy -Scope Process -Force
 
-Write-Output "Script execution completed successfully."
+Log-Message "Script execution completed successfully."
