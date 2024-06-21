f361b855e086e2905280d062ba552b58ccf17f58
Stefanfrijns <153369666+Stefanfrijns@users.noreply.github.com>
2024-06-21 10:37:01 +0200

    Update ConfigureNetwork.ps1

--- infra/linux/virtualbox/ConfigureNetwork.ps1
+++ infra/linux/virtualbox/ConfigureNetwork.ps1
@@ -1,99 +1,182 @@
 param (
     [string]$VMName,
-    [string]$NetworkType
+    [string]$NetworkType,
+    [string]$AdapterName,
+    [string]$SubnetNetwork,
+    [int]$NicIndex
 )
 
 # Validate input parameters
-if (-not $VMName -or -not $NetworkType) {
-    throw "All parameters must be provided: VMName, NetworkType"
+if (-not $VMName -or -not $NetworkType -or -not $AdapterName -or (-not $SubnetNetwork -and $NetworkType -ne "bridged") -or -not $NicIndex) {
+    throw "All parameters must be provided: VMName, NetworkType, AdapterName, SubnetNetwork (except for bridged), NicIndex"
 }
 
 # Path to VBoxManage
 $vboxManagePath = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
-
-##########Functions###########
-
-$logFilePath = "$env:Public\ConfigureNetwork.log"
-function Log-Message {
-    param (
-        [string]$message
-    )
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $logMessage = "$timestamp - $message"
-    Write-Output $logMessage
-    Add-Content -Path $logFilePath -Value $logMessage
-}
-
-# Function to get available bridged network adapters
-function Get-BridgedNetworkAdapters {
-    $adapters = & "$vboxManagePath" list bridgedifs | Select-String -Pattern "Name: " | ForEach-Object { $_.Line.Split(":")[1].Trim() }
-    if ($adapters.Count -eq 0) {
-        throw "No bridged adapters found."
-    }
-    return $adapters[0]  # Return the first available adapter
-}
-
-# Function to get available host-only network adapters
-function Get-HostOnlyNetworkAdapters {
-    $adapters = & "$vboxManagePath" list hostonlyifs | Select-String -Pattern "Name: " | ForEach-Object { $_.Line.Split(":")[1].Trim() }
-    return $adapters
-}
+$publicFolderPath = "$env:Public\VMNetworkConfigurations"
+$controlFilePath = "$publicFolderPath\controlehostonly.txt"
 
 # Function to create a host-only network adapter
 function Create-HostOnlyAdapter {
-    $output = & "$vboxManagePath" hostonlyif create 2>&1
-    if ($output -match "Interface '(\S+)' was successfully created") {
-        return $matches[1]
+    $output = & "$vboxManagePath" hostonlyif create | Out-String
+
+    if ($output -match "Interface 'VirtualBox Host-Only Ethernet Adapter #(\d+)' was successfully created") {
+        $adapterNumber = $matches[1]
+        $adapterName = "VirtualBox Host-Only Ethernet Adapter #$adapterNumber"
+        return $adapterName
+    } else {
+        throw "Failed to create host-only adapter: $output"
     }
-    Log-Message "Failed to create host-only adapter: $output"
-    throw "Failed to create host-only adapter."
 }
 
-# Function to configure network
-function Configure-Network {
+# Function to configure IP for host-only adapter
+function Configure-HostOnlyAdapterIP {
     param (
-        [string]$VMName,
-        [string]$NetworkType
+        [string]$adapterName,
+        [string]$SubnetNetwork
     )
-    switch ($NetworkType) {
-        "host-only" {
-            $adapters = Get-HostOnlyNetworkAdapters
-            if ($adapters.Count -eq 0) {
-                Log-Message "No host-only adapters found. Creating one..."
-                $adapter = Create-HostOnlyAdapter
-                Log-Message "Created host-only adapter $adapter"
+
+    # Extract network and subnet mask
+    if ($SubnetNetwork -match "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,2})$") {
+        $network = $matches[1]
+        $cidr = [int]$matches[2]
+
+        # Convert CIDR to subnet mask
+        $subnetMask = [string]::Join('.', (0..3 | ForEach-Object {
+            if ($cidr -ge ($_ + 1) * 8) {
+                255
+            } elseif ($cidr -le $_ * 8) {
+                0
             } else {
-                $adapter = $adapters[0]
+                256 - [math]::pow(2, 8 - ($cidr % 8))
             }
-            & "$vboxManagePath" modifyvm $VMName --nic1 hostonly --hostonlyadapter1 $adapter
-            Log-Message "Configured host-only network for $VMName using adapter $adapter"
+        }))
+    } else {
+        throw "Invalid SubnetNetwork format. Expected format is 'x.x.x.x/x'"
+    }
+
+    $ipconfigOutput = & "$vboxManagePath" hostonlyif ipconfig "$adapterName" --ip "$network" --netmask "$subnetMask"
+    Write-Output "hostonlyif ipconfig output: $ipconfigOutput"
+
+    if ($ipconfigOutput -notmatch "successfully configured") {
+        throw "Failed to configure IP for adapter ${adapterName}: $ipconfigOutput"
+    }
+}
+
+# Function to get the course name from the VM name
+function Get-CourseName {
+    param (
+        [string]$vmName
+    )
+    return ($vmName -split '_')[0]
+}
+
+# Save network configuration to file
+function Save-NetworkConfiguration {
+    param (
+        [string]$courseName,
+        [string]$VMName,
+        [string]$OriginalAdapterName,
+        [string]$ActualAdapterName
+    )
+    $courseConfigFile = "$publicFolderPath\NetworkConfig_$courseName.json"
+
+    if (-not (Test-Path $publicFolderPath)) {
+        New-Item -ItemType Directory -Path $publicFolderPath -Force
+    }
+
+    $networkConfig = @()
+    if (Test-Path $courseConfigFile) {
+        $networkConfig = (Get-Content -Path $courseConfigFile -Raw | ConvertFrom-Json)
+        if (-not ($networkConfig -is [System.Collections.ArrayList])) {
+            $networkConfig = @($networkConfig)
         }
-        "nat" {
-            & "$vboxManagePath" modifyvm $VMName --nic1 nat
-            Log-Message "Configured NAT network for $VMName"
+    }
+
+    $networkConfig += [PSCustomObject]@{
+        VMName              = $VMName
+        OriginalAdapterName = $OriginalAdapterName
+        ActualAdapterName   = $ActualAdapterName
+    }
+
+    $networkConfig | ConvertTo-Json -Compress | Set-Content -Path $courseConfigFile
+    Write-Output "Network configuration saved to $courseConfigFile"
+}
+
+# Function to get the actual adapter name from the configuration file
+function Get-ActualAdapterName {
+    param (
+        [string]$courseName,
+        [string]$OriginalAdapterName
+    )
+    $courseConfigFile = "$publicFolderPath\NetworkConfig_$courseName.json"
+
+    if (Test-Path $courseConfigFile) {
+        $configContent = Get-Content -Path $courseConfigFile -Raw | ConvertFrom-Json
+        $config = $configContent | Where-Object { $_.OriginalAdapterName -eq $OriginalAdapterName }
+        if ($config) {
+            return $config.ActualAdapterName
         }
-        "bridged" {
-            $adapter = Get-BridgedNetworkAdapters
-            & "$vboxManagePath" modifyvm $VMName --nic1 bridged --bridgeadapter1 $adapter
-            Log-Message "Configured bridged network for $VMName using adapter $adapter"
-        }
-        default {
+    }
+    return $null
+}
+
+# Ensure a host-only adapter is always created initially if the control file is empty
+if (-not (Test-Path $controlFilePath) -or (Get-Content -Path $controlFilePath -Raw).Trim() -eq '') {
+    try {
+        Write-Output "Creating initial host-only adapter for control purposes..."
+        Create-HostOnlyAdapter | Out-Null
+    } catch {
+        Write-Output "An error occurred while creating the initial host-only adapter: $($_.Exception.Message)"
+    } finally {
+        Set-Content -Path $controlFilePath -Value '1'
+    }
+}
+
+try {
+    Write-Output "Starting network configuration for $VMName with network type $NetworkType"
+    
+    $courseName = Get-CourseName -vmName $VMName
+    $actualAdapterName = Get-ActualAdapterName -courseName $courseName -OriginalAdapterName $AdapterName
+
+    if ($actualAdapterName) {
+        Write-Output "Using existing adapter $actualAdapterName for $VMName"
+    } else {
+        if ($NetworkType -eq "host-only") {
+            $actualAdapterName = Create-HostOnlyAdapter
+            Configure-HostOnlyAdapterIP -adapterName $actualAdapterName -SubnetNetwork $SubnetNetwork
+            Save-NetworkConfiguration -courseName $courseName -VMName $VMName -OriginalAdapterName $AdapterName -ActualAdapterName $actualAdapterName
+        } elseif ($NetworkType -eq "natnetwork") {
+            $natNetName = "NatNetwork_$AdapterName"
+            Write-Output "Adding NAT network with name $natNetName and network $SubnetNetwork"
+            & "$vboxManagePath" natnetwork add --netname $natNetName --network $SubnetNetwork --dhcp off
+            $actualAdapterName = $natNetName
+            Save-NetworkConfiguration -courseName $courseName -VMName $VMName -OriginalAdapterName $AdapterName -ActualAdapterName $actualAdapterName
+        } elseif ($NetworkType -eq "bridged") {
+            $actualAdapterName = $AdapterName
+            Save-NetworkConfiguration -courseName $courseName -VMName $VMName -OriginalAdapterName $AdapterName -ActualAdapterName $actualAdapterName
+        } else {
             throw "Unsupported network type: $NetworkType"
         }
     }
-}
 
-##########EXECUTE###########
+    # Configure the network adapters
+    if ($NetworkType -eq "host-only") {
+        Write-Output "Configuring host-only network for $VMName using adapter $actualAdapterName"
+        & "$vboxManagePath" modifyvm $VMName --nic$NicIndex hostonly --hostonlyadapter$NicIndex $actualAdapterName
+    } elseif ($NetworkType -eq "natnetwork") {
+        Write-Output "Configuring NAT network for $VMName using network $actualAdapterName"
+        & "$vboxManagePath" modifyvm $VMName --nic$NicIndex natnetwork --nat-network$NicIndex $actualAdapterName
+    } elseif ($NetworkType -eq "bridged") {
+        Write-Output "Configuring bridged network for $VMName using adapter $actualAdapterName"
+        & "$vboxManagePath" modifyvm $VMName --nic$NicIndex bridged --bridgeadapter$NicIndex $actualAdapterName
+    }
 
-# Call the function to configure the network
-try {
-    Log-Message "Starting network configuration for $VMName with network type $NetworkType"
-    Configure-Network -VMName $VMName -NetworkType $NetworkType
-    Log-Message "Network configuration completed for $VMName"
-}
-catch {
-    Log-Message "An error occurred: $($_.Exception.Message)"
+    Start-Sleep -Seconds 2  # Pause for 2 seconds to avoid lock issues
+    Write-Output "Network configuration completed for $VMName"
+} catch {
+    Write-Output "An error occurred: $($_.Exception.Message)"
     throw
 }
 
-Log-Message "Script execution completed successfully."
+Write-Output "Script execution completed successfully."
