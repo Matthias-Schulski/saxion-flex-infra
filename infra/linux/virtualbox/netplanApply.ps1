c073bb992706931161303d0ec3959e58aa1432e2
LucaKasanardjo <118907054+LucaKasanardjo@users.noreply.github.com>
2024-06-23 16:10:37 +0200

    Update netplanApply.ps1

--- infra/linux/virtualbox/netplanApply.ps1
+++ infra/linux/virtualbox/netplanApply.ps1
@@ -1,14 +1,75 @@
-﻿### PARAMETERS
+### PARAMETERS
 param (
 [STRING]$username = "",         #DEFAULT USER
 [STRING]$password = "",         #DEFAULT WACHTWOORD
 [STRING]$hostname = "",
 [STRING]$vmname   = ""
 )
+    $downloadsPath = "C:\Users\Public\Downloads"
+    $localNetplanPath = "$downloadsPath\50-cloud-init.yaml"
+    #POSH-SSH CONFIGUREREN
+    $SecurePassword = ConvertTo-SecureString -String "$password" -AsPlainText -Force
+    $Credential = New-Object -TypeName PSCredential -ArgumentList $Username, $SecurePassword
+    
+    #CHECK OF VM IS OPGESTART
+    $VMStarted = $false
+        while (-not $VMStarted) {
+            Start-Sleep -Seconds 5
+            $VMInfo = & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" showvminfo $VMName --machinereadable
+            # Controleer de status van de VM
+            foreach ($line in $VMInfo) {
+                if ($line -match 'VMState="running"') {
+                    $VMStarted = $true
+                    break
+                    }
+            }
+            if (-not $VMStarted) {
+                Write-Host "Wachten tot de VM is opgestart..."
+        }
+    }
 
-write-host "NU WORDT HET NETPLAN OVERGEKOPIEERD EN TOEGEPAST OP $vmname" -ForegroundColor DarkRed
-VboxManage guestcontrol $vmname mkdir "/home/$hostname/netplan" --username $Username --password $Password
-$localNetplanPath = (Join-Path -Path $env:USERPROFILE -ChildPath "\VirtualBox VMs\$VMName-netplan")
+    Write-Host "VM is opgestart en draait."
 
-Write-Host "NETPLAN OVERKOPIEREN" -ForegroundColor DarkRed
-VBoxManage guestcontrol $vmname copyto "$localNetplanPath\50-cloud-init.yaml"  --target-directory "/home/$hostname/netplan/50-cloud-init.yaml" --username $username --password $password
+    #CHECK OF SSH MOGELIJK IS
+    $SSHAvailable = $false
+    $sshCounter = 0
+        while (-not $SSHAvailable) {
+            Start-Sleep -Seconds 10
+            try {            
+                $testSSH = New-SSHSession -ComputerName "127.0.0.1" -Port $hostport -Credential $credential
+            if ($testSSH.SessionId -ne $null) {
+                $SSHAvailable = $true
+                Remove-SSHSession -SessionId $testSSH.SessionId
+            }
+            } catch {
+                $sshcounter++
+                Write-Host "$sshcounter - Wachten tot SSH beschikbaar is..."
+            }
+    }
+
+    Write-Host "SSH is beschikbaar." -ForegroundColor green
+    
+    $SSHSession = New-SSHSession -ComputerName "127.0.0.1" -Port $hostport -Credential $credential
+
+    if ($SSHSession -ne $null) {
+    Write-Host "SSH sessie succesvol aangemaakt. SessionId: $($SSHSession.SessionId)" -ForegroundColor yellow
+    
+    # Voer een commando uit
+    $CommandResult = invoke-sshcommand -sessionid $sshsession.sessionid -command "sudo apt install -y bzip2 tar; sudo mount /dev/cdrom /mnt; cd /mnt; sudo sh ./VBoxLinuxAdditions.run"    
+    # Output de resultaten van het commando
+    Write-Host "Command executed. Result: $($CommandResult.Output)"
+    
+    # Sluit de SSH sessie
+    Remove-SSHSession -SessionId $SSHSession.SessionId
+    } else {
+        Write-Host "Fout bij het aanmaken van de SSH sessie." -ForegroundColor DarkRed
+        break
+    }
+
+    write-host "NU WORDT HET NETPLAN OVERGEKOPIEERD EN TOEGEPAST OP $vmname" -ForegroundColor DarkRed
+    VboxManage guestcontrol $vmname mkdir "/home/$hostname/netplan" --username $Username --password $Password
+
+    Write-Host "NETPLAN OVERKOPIEREN EN UITVOEREN" -ForegroundColor DarkRed
+    VBoxManage guestcontrol $vmname copyto "$localNetplanPath\50-cloud-init.yaml"  --target-directory "/home/$hostname/netplan/50-cloud-init.yaml" --username $username --password $password
+    VBoxManage guestcontrol $vmname execute --image "/bin/bash" --username $username --password $password --wait-exit --wait-stdout --verbose -- /bin/bash -c "sudo mv '/home/$hostname/netplan/50-cloud-init.yaml' '/etc/netplan/50-cloud-init.yaml"
+    VBoxManage guestcontrol $vmname execute --image "/bin/bash" --username $username --password $password --wait-exit --wait-stdout --verbose -- /bin/bash -c "sudo netplan apply"
