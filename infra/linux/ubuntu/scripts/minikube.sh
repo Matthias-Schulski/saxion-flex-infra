a73dd58375ba64dbb94b2fbb48463a0b4066f41d
LucaKasanardjo <118907054+LucaKasanardjo@users.noreply.github.com>
2024-06-26 12:54:15 +0200

    Update minikube.sh

--- infra/linux/ubuntu/scripts/minikube.sh
+++ infra/linux/ubuntu/scripts/minikube.sh
@@ -7,14 +7,14 @@ mkdir ~/MK
 #INSTALLEER KUBECTL
 echo "Kubectl installeren"
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o kubectl
-sudo install -o root -g root -m 0755 ~/scripts/minikube/kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 ~/scripts/kubectl /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl   # Zorg ervoor dat kubectl uitvoerbaar is
 kubectl version --client
 
 #INSTALLEER MINIKUBE
 echo "Minikube installeren"
 sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o ~/MK/minikube-linux-amd64
-sudo install -o root -g root -m 0755 ~/scripts/minikube/minikube-linux-amd64 /usr/local/bin/minikube
+sudo install -o root -g root -m 0755 ~/scripts/minikube-linux-amd64 /usr/local/bin/minikube
 sudo chmod +x /usr/local/bin/minikube   # Zorg ervoor dat minikube uitvoerbaar is
 minikube version
 
