544ff2777f1f6594c2d2f608db679a0a79e3d782
LucaKasanardjo <118907054+LucaKasanardjo@users.noreply.github.com>
2024-06-27 15:43:43 +0200

    Update netplanApply.ps1

--- infra/linux/virtualbox/netplanApply.ps1
+++ infra/linux/virtualbox/netplanApply.ps1
@@ -51,7 +51,7 @@ param (
         )
 
         #SSH SESSIE MAKEN MET POSH-SSH
-        $SSHSession = New-SSHSession -ComputerName $ComputerName -Port $Port -Credential $Credential -AcceptKey
+        $SSHSession = New-SSHSession -ComputerName $ComputerName -Port $Port -Credential $Credential -AcceptKey -Force
 
         #CHECKEN OF SSHSESSIE CORRECT IS AANGEMAAKT
         if ($SSHSession -ne $null) 
